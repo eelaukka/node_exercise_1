77b9072ea5d0ca9548f12c71c975e3d38a7f2ba8
eelaukka <eelaukka@student.oulu.fi>
2017-10-10 12:43:41 +0300

    greetings

---
+++ greeter.js
@@ -0,0 +1,10 @@
+var greetings = new Array();
+
+greetings.push("Moro");
+greetings.push("Moi");
+greetings.push("Terve");
+greetings.push("Bonjourno");
+greetings.push("Bonjour");
+
+
+console.log(greetings[Math.floor(Math.random()*5)]);
